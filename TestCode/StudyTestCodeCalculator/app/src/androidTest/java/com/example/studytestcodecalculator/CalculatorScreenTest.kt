ac7018231bd29d6d403eea323961c33dbae08823
이정현 <cw03125@naver.com>
2025-03-28 15:38:10 +0900

    Compose-UI-Test 적용

---
+++ TestCode/StudyTestCodeCalculator/app/src/androidTest/java/com/example/studytestcodecalculator/CalculatorScreenTest.kt
@@ -0,0 +1,45 @@
+package com.example.studytestcodecalculator
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.studytestcodecalculator.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CalculatorScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun whenAddButtonClicked_resultIsCorrect(){
+        // 숫자 입력
+        composeTestRule.onNodeWithText("Number A").performTextInput("3")
+        composeTestRule.onNodeWithText("Number B").performTextInput("5")
+        // 버튼 클릭
+        composeTestRule.onNodeWithText("+").performClick()
+        // 결과 확인
+
+        composeTestRule.onNodeWithText("Result: 8.0").assertIsDisplayed()
+    }
+
+    @Test
+    fun whenDivideByZero_resultIsInvalidInput(){
+        composeTestRule.onNodeWithText("Number A").performTextInput("10")
+        composeTestRule.onNodeWithText("Number B").performTextInput("0")
+
+        composeTestRule.onNodeWithText("÷").performClick()
+
+        composeTestRule
+            .onNodeWithText("Result: Invalid Input")
+            .assertIsDisplayed()
+    }
+
+
+}
