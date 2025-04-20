03f328f475c7f9712dea88b542e87113f72c96d7
이정현 <cw03125@naver.com>
2025-04-20 15:46:00 +0900

    Red 실패 테스트 케이스 작성

---
+++ TestCode/StudyTestDiscountCalc/app/src/test/java/study/test/discountcalc/DiscountCalculatorTest.kt
@@ -0,0 +1,17 @@
+package study.test.discountcalc
+
+import org.junit.Test
+
+class DiscountCalculatorTest {
+
+    @Test
+    fun 원가_10000원에서_20퍼센트_할인시_8000원이_나와야함(){
+        val calculator = DiscountCalculator()
+        val result = calculator.calculatorFinalPrice(
+            originalPrice = 10000,
+            discountPercent = 20.0
+        )
+
+        assertEquals(8000,result)
+    }
+}
